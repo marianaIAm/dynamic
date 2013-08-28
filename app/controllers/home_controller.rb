ccc575fd5515afb399211d6dd3367b919d040211
Mariana Lenetis <mlenetis@gmail.com>
2013-08-28 14:24:32 -0700

    Add ability to detect Admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if is_admin?
+      puts "****************ADMIN***************"
+    else
+      puts "*****************NOT ADMIN***********"
+    end
   end
 end
