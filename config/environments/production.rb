b60dfe509e6f509ff63299b047e44ef876710309
Mariana Lenetis <mlenetis@gmail.com>
2013-08-28 12:47:45 -0700

    Install and configure Devise gem, add Home controller, index action, view and route

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Dynamic::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Add for Devise Authentication
+  config.action_mailer.default_url_options = { :host => 'heroku' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
