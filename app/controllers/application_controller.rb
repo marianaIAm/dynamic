b905809c1dbfac5b9cd5c2a3635f3bdcd5923c8b
Mariana Lenetis <mlenetis@gmail.com>
2013-08-30 10:57:21 -0700

    Clean up old comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,6 @@ class ApplicationController < ActionController::Base
   private
 
   def current_trainer_id
-   # '29'
-    #trainer_id = Trainer.select(:id).where(account_id: 58)
     trainer_id = Trainer.select(:id).where(account_id: current_account)
 
   end
